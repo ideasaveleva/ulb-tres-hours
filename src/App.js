623d81fb420be4104c0eb8dbe5f280dce56876a4
Katerina Saveleva <ash2789@mail.ru>
2022-11-13 08:48:40 -0600

    add PostList and props title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import { Counter } from './components/Counter';
-import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 import './styles/App.css';
 
 function App() {
@@ -9,14 +9,17 @@ function App() {
     { id: 1, title: 'JavaScript', body: 'Description' },
     { id: 2, title: 'JavaScript 2', body: 'Description' },
     { id: 3, title: 'JavaScript 3', body: 'Description' },
+	]);
+	const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' },
   ]);
 
   return (
-		<div className="App">
-			<h1 style={{textAlign: 'center'}}>Список постов</h1>
-      {posts.map((post) => (
-				<PostItem post={post} key={post.id} />
-      ))}
+    <div className="App">
+      <PostList posts={posts} title="Список постов 1" />
+      <PostList posts={posts2} title="Список постов 2" />
     </div>
   );
 }
