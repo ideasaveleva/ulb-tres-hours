9e2cae482ebf430c2f9081c1dc353a5b60b69c5a
Katerina Saveleva <ash2789@mail.ru>
2022-11-13 08:30:55 -0600

    add component PostItem and styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import { Counter } from './components/Counter';
+import PostItem from './components/PostItem';
+import './styles/App.css';
 
 function App() {
-  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
-
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  ]);
 
   return (
-    <div className="App">
-			<ClassCounter/>
+		<div className="App">
+			<h1 style={{textAlign: 'center'}}>Список постов</h1>
+      {posts.map((post) => (
+				<PostItem post={post} key={post.id} />
+      ))}
     </div>
   );
 }
