365faaf52be5df8372340f9eb21b8d0cc9e23583
Katerina Saveleva <ash2789@mail.ru>
2022-11-13 17:07:34 -0600

    add PostForm, реализ. функция обратн.выз. к родит.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import { useState, useRef } from 'react';
-import ClassCounter from './components/ClassCounter';
-import { Counter } from './components/Counter';
+import { useState } from 'react';
 import PostList from './components/PostList';
-import { MyButton } from './components/UI/button/MyButton';
-import { MyInput } from './components/UI/input/MyInput';
+import { PostForm } from './components/PostForm';
 import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'JavaScript 2', body: 'Description' },
-    { id: 3, title: 'JavaScript 3', body: 'Description' },
-  ]);
-
-  const [post, setPost] = useState({ title: '', body: '' });
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}]);
-    setPost({ title: '', body: '' });
-  };
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'JavaScript', body: 'Description' },
+		{ id: 2, title: 'JavaScript 2', body: 'Description' },
+		{ id: 3, title: 'JavaScript 3', body: 'Description' },
+	]);
+	
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost])
+	}
 
   return (
     <div className="App">
-      <form>
-        {/*Управляемый компоненнт*/}
-        <MyInput
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          type="text"
-          placeholder="Название поста"
-        />
-        <MyInput
-          value={post.body}
-          onChange={(e) => setPost({ ...post, body: e.target.value })}
-          type="text"
-          placeholder="Описание поста"
-        />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
+			<PostForm create={createPost} />
       <PostList posts={posts} title="Посты про JS" />
     </div>
   );
