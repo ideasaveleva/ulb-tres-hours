6ce65c3cbc5906564598502aa9506ddb0f99f3ce
Katerina Saveleva <ash2789@mail.ru>
2022-11-13 09:15:26 -0600

    add MyButton and module.css, props children

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import { Counter } from './components/Counter';
 import PostList from './components/PostList';
+import { MyButton } from './components/UI/button/MyButton';
 import './styles/App.css';
 
 function App() {
@@ -10,16 +11,16 @@ function App() {
     { id: 2, title: 'JavaScript 2', body: 'Description' },
     { id: 3, title: 'JavaScript 3', body: 'Description' },
 	]);
-	const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' },
-  ]);
+	
 
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title="Посты про JS" />
-      <PostList posts={posts2} title="Посты про Python" />
     </div>
   );
 }
