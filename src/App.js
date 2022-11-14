024a67926411cfceca6eb62168fc8726ea493906
Katerina Saveleva <ash2789@mail.ru>
2022-11-13 18:28:16 -0600

    реализовано удаление постов через 2 ребенка

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import { PostForm } from './components/PostForm';
 import './styles/App.css';
 
 function App() {
-	const [posts, setPosts] = useState([
-		{ id: 1, title: 'JavaScript', body: 'Description' },
-		{ id: 2, title: 'JavaScript 2', body: 'Description' },
-		{ id: 3, title: 'JavaScript 3', body: 'Description' },
-	]);
-	
-	const createPost = (newPost) => {
-		setPosts([...posts, newPost])
-	}
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  ]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  //Получаем post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
 
   return (
     <div className="App">
-			<PostForm create={createPost} />
-      <PostList posts={posts} title="Посты про JS" />
+      <PostForm create={createPost} />
+      {posts.length ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
+      )}
     </div>
   );
 }
